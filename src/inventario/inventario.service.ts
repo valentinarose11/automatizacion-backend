0dde15333f214096157ddc38bb8db14206117af3
Valentina Rosales <valentina.rosales@correounivalle.edu.co>
2020-10-30 15:24:16 -0500

    Mejora en la creacion del invenario

--- src/inventario/inventario.service.ts
+++ src/inventario/inventario.service.ts
@@ -36,11 +36,27 @@ export class InventarioService {
     ]
   }
 
-  create(createInventarioDto: CreateInventarioDto): Promise<Inventario> {
-    const inventario = new Inventario();
-    inventario.materia_prima_id = createInventarioDto.materia_prima_id;
+  async create(createInventarioDto: CreateInventarioDto): Promise<Inventario> {
+    let inventarioDB = await this.inventarioModel.findOne({
+      where: {
+        materia_prima_id: createInventarioDto.materia_prima_id
+      }
+    })
+    if(inventarioDB) {
+      return await this.updateInvetario(inventarioDB,createInventarioDto);
+    } else {
+      const inventario = new Inventario();
+      inventario.materia_prima_id = createInventarioDto.materia_prima_id;
+      inventario.cantidad = createInventarioDto.cantidad;
+      await inventario.save();
+      return this.findOne(inventario.id)
+    }
+  }
+
+  async updateInvetario(inventario: Inventario,createInventarioDto: CreateInventarioDto) {
     inventario.cantidad = createInventarioDto.cantidad;
-    return inventario.save();
+    await inventario.save();
+    return this.findOne(inventario.id)
   }
 
   async findAll(): Promise<Inventario[]> {
@@ -60,10 +76,7 @@ export class InventarioService {
   async update(id: string, createInventarioDto: CreateInventarioDto) {
     const inventario = await this.findOne(id);
     if (inventario) {
-      inventario.materia_prima_id = createInventarioDto.materia_prima_id ||inventario.materia_prima_id;
-      inventario.cantidad = createInventarioDto.cantidad ||inventario.cantidad;
-      await inventario.save();
-      return this.findOne(id);
+      return await this.updateInvetario(inventario, createInventarioDto);
     } else {
       throw new NotFoundException({ error: "ID no existe", status: 404 }, "ID no existe");
     }
